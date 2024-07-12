6f28a9cbf96eae66bbed7cd6b30635dfb760375c
karinatimm <kartimm.webdev@gmail.com>
2024-07-12 17:31:30 +0200

    Updated src/containers/MovieDetails.jsx

--- src/containers/MovieDetails.jsx
+++ src/containers/MovieDetails.jsx
@@ -55,7 +55,7 @@ const MovieDetails = ({ selectedId, onCloseMovie, onAddWatched, watched }) => {
     const getMovieDetails = async () => {
       setIsLoading(true);
       const res = await fetch(
-        `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+        `https://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
       );
       const data = await res.json();
 
